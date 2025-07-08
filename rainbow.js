0af0f1b5a74992c45df8fd7a2d62a5a8c05bf0c7
Anoshkin <yanoshkin@wpi.edu>
2025-07-08 15:33:54 -0400

    started to learn js

---
+++ rainbow.js
@@ -0,0 +1,28 @@
+console.log("hello world")
+
+let element = document.getElementById("tswcontainer")
+element.style.opacity = 1
+let angle = 0;
+let shouldrainbow = true;
+console.log(element)
+function onframe() {
+    angle += 1;
+    let string = "hsl(" + angle + ", 100%, 50%)"
+    if (shouldrainbow) {
+        element.style.backgroundColor = string
+    }
+    console.log(string)
+    requestAnimationFrame(onframe)
+}
+onframe()
+
+function start_rainbow() {
+    shouldrainbow = true;
+
+}
+
+function onlmb (event) {
+    shouldrainbow = false;
+    element.style.backgroundColor = input.value;
+    setTimeout(start_rainbow, 1000)
+}
