50145cb8963e67963299aa8ebb585395c9288173
Anoshkin <yanoshkin@wpi.edu>
2025-07-10 14:18:22 -0400

    clickgame

---
+++ clickgame-broken/game.js
@@ -0,0 +1,88 @@
+const widget_container = document.getElementById("widget-container");
+const stores = document.getElementsByClassName("store");
+const score_element = document.getElementById("score");
+let score = 5;
+let super_gompei_count = 0;
+
+function changeScore(amount) {
+    score += amount;
+    //set score element
+
+    // Update the stores to show ones that are too expensive
+    for (let store of stores) {
+        let cost = parseInt(store.getAttribute("cost"));
+
+        if (score > cost) {
+            store.setAttribute("broke", "");
+        } else {
+            store.removeAttribute("broke");
+        }
+    }
+}
+function buy(store) {
+    const cost = parseInt(store.getAttribute("cost"));
+
+    // check available to buy
+    // change score
+
+    if (store.getAttribute("name") === "Super-Gompei") {
+        const super_gompei = document.querySelector("#widget-container #super-gompei")?.parentElement;
+        // If Super-Gompei already exists
+        if (super_gompei) {
+            super_gompei.setAttribute("reap", (parseInt(super_gompei.getAttribute("reap")) + 100));
+            return;
+        }
+    }
+
+    super_gompei_count += 1;
+    document.body.style = "--gompei-count: " + super_gompei_count + ";"
+
+    // clone node for widget, and add to container
+    const widget = store.firstElementChild.cloneNode(true);
+    widget.onclick = () => {
+        harvest(widget);
+    }
+    widget_container.appendChild(widget);
+
+    if (widget.getAttribute("auto") == 'true') {
+        widget.setAttribute("harvesting", "");
+        setup_end_harvest(widget);
+    }
+}
+
+function setup_end_harvest(widget) {
+    setTimeout(() => {
+        // Remove the harvesting flag
+        widget.removeAttribute("harvesting");
+        // If automatic, start again
+        if (widget.getAttribute("auto") == 'true') {
+            harvest(widget);
+        }
+    }, parseFloat(widget.getAttribute("cooldown")) * 1000);
+}
+
+function harvest(widget) {
+    // Only run if currently not harvesting
+    if (widget.hasAttribute("harvesting")) return;
+    // Set harvesting flag
+    widget.setAttribute("harvesting", "");
+
+    // If manual, collect points now
+    changeScore(parseInt(widget.getAttribute("reap")));
+    showPoint(widget);
+
+    setup_end_harvest(widget);
+}
+
+
+function showPoint(widget) {
+    let number = document.createElement("span");
+    number.className = "point";
+    number.innerHTML = "+" + widget.getAttribute("reap");
+    number.onanimationend = () => {
+        number.removeChild(widget);
+    }
+    widget.appendChild(number);
+}
+
+changeScore(0);
